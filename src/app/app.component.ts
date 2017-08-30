10dc6cdfa5c1d4dd4106de63353a0d9c239da73c
qaje <javierpal3232@gmail.com>
2017-08-30 23:37:33 +0000

    component add JQuery

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+declare var JQuery: any;
+declare var $: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Web  de Angular 4 con Angular CLI corriendo app !!';
+
+  public toggleTitle(){
+	  console.log('Le has dado click en el boton');
+	  //usar JQUERY
+	  $('.title').slideToggle();
+  }
 }
